43652485544c4b3587100fa9d0a4215db77d2ae1
Z User <z@container>
2026-09-12 05:19:01 +0000

    auto-sync: perubahan aplikasi 2026-09-12 05:19:01

--- scripts/tmp/get-codes.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const db = new PrismaClient();
-const apps = await db.application.findMany({
-  select: { name: true, trackingCode: true, status: true, offerStatus: true },
-});
-console.log(JSON.stringify(apps, null, 1));
-await db.$disconnect();
